c86eef3c65d8c6d5609def152cf23f192c502fbb
K.J_Hennigan <imnotevenangrybro@gmail.com>
2025-11-08 21:19:37 -0600

    Enable grid and add platforms and collectables

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,7 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "#97da60ff"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -27,21 +27,43 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
+    createPlatform(200, 100, 300, 59, "#ffffffff" );
+    createPlatform(250, 150, 300, 59, "#ffffffff" );
+      createPlatform(1200, 69, 150, 150, "#deeb2fff" );
+    createPlatform(1000, 100, 300, 59, "#ffffffff" );
+    createPlatform(950, 150, 300, 59, "#ffffffff" );  
+    createPlatform(150, 650, 300, 400, "#a16d3bff" );
+    createPlatform(450, 600, 350, 400, "#a16d3bff" );
+    createPlatform(1050, 600, 350, 400, "#926236ff" );
+    createPlatform(1050, 600, 350, 30, "#97da60ff" );
+    createPlatform(800, 720, 600, 400, "#3b42a1ff" );
+    createPlatform(150, 650, 300, 30, "#97da60ff" );
 
+    createPlatform(450, 570, 350, 30, "#82b856ff" );
 
 
 
     // TODO 3 - Create Collectables
-
+    createCollectable("database", 200, 170, 0.5, 0.7);
+    createCollectable("database", 700, 170, 0.5, 0.7);
+    createCollectable("database", 930, 370);
 
 
     
     // TODO 4 - Create Cannons
 
+    createCannon("top", 900, 3000);
+    createCannon("right", 500, 3000);
+    createCannon("bottom", 200, 3000);
+    createCannon("bottom", 800, 3000);
+
+
+
+    
 
     
     
@@ -52,3 +74,4 @@ $(function () {
 
   registerSetup(setup);
 });
+
